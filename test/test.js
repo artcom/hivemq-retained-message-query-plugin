3ffe957b7d3e1139140ede22fa23f124d90e9523
Guido Lorenz <guido.lorenz@artcom.de>
2015-06-17 12:19:35 +0200

    Add test to query two topics at once

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ const _ = require("lodash");
 describe("HTTP API", function() {
   const brokerUrl = process.env.BROKER || "localhost";
   const prefix = `test/hivemq-api-${Date.now()}`;
-  const topics = {
+  const data = {
     [`${prefix}/topic1`]: "foo",
     [`${prefix}/topic2`]: "bar"
   };
@@ -21,13 +21,13 @@ describe("HTTP API", function() {
   });
 
   beforeEach(function() {
-    _.forEach(topics, (value, topic) => {
+    _.forEach(data, (value, topic) => {
       client.publish(topic, value, { retain: true });
     });
   });
 
   afterEach(function() {
-    _.forEach(topics, (value, topic) => {
+    _.forEach(data, (value, topic) => {
       client.publish(topic, null, { retain: true });
     });
   });
@@ -41,9 +41,18 @@ describe("HTTP API", function() {
   };
 
   it("should return the value of a topic", function() {
-    const [topic, value] = _(topics).pairs().first();
+    const [topic, value] = _(data).pairs().first();
     return sendQuery({ topic }).then((result) => {
       expect(result).to.equal({ topic, value });
+    });
+  });
+
+  it("should return the values of multiple topics", function() {
+    const topics = _.keys(data);
+    const expected = _.map(topics, (topic) => ({ topic, value: data[topic] }));
+
+    return sendQuery({ topics }).then((results) => {
+      expect(results).to.equal(expected);
     });
   });
 });
