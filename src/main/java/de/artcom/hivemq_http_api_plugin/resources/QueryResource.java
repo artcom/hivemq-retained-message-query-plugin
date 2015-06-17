8f51ee34f0fbc532a314e4ee3284d4c2a43bbeea
Guido Lorenz <guido.lorenz@artcom.de>
2015-06-17 16:51:07 +0200

    Implement first test case

--- src/main/java/de/artcom/hivemq_http_api_plugin/resources/QueryResource.java
+++ src/main/java/de/artcom/hivemq_http_api_plugin/resources/QueryResource.java
@@ -1,22 +1,24 @@
 package de.artcom.hivemq_http_api_plugin.resources;
 
+import com.dcsquare.hivemq.spi.message.RetainedMessage;
 import com.dcsquare.hivemq.spi.services.RetainedMessageStore;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
-@Path("query")
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static javax.ws.rs.core.Response.Status.OK;
+
 public class QueryResource {
 
     private static final Logger LOG = LoggerFactory.getLogger(QueryResource.class);
@@ -31,24 +33,44 @@ public class QueryResource {
     }
 
     @POST
-    public String query(String body) {
+    @Path("query")
+    public Response query(String body) {
         try {
             Query query = objectMapper.readValue(body, Query.class);
-            return query.topic;
+            Optional<QueryResult> result = doQuery(query.topic);
+
+            if (!result.isPresent()) {
+                return Response.status(NOT_FOUND).build();
+            }
+
+            String json = objectMapper.writeValueAsString(result.get());
+            return Response.status(OK).entity(json).build();
         } catch (IOException e) {
-            return "error";
+            return Response.status(BAD_REQUEST).build();
         }
     }
 
-    @XmlRootElement
+    private Optional<QueryResult> doQuery(String topic) {
+        Optional<RetainedMessage> optionalMessage = retainedMessageStore.getRetainedMessage(topic);
+
+        if (!optionalMessage.isPresent()) {
+            return Optional.absent();
+        }
+
+        RetainedMessage message = optionalMessage.get();
+        QueryResult result = new QueryResult();
+        result.topic = message.getTopic();
+        result.value = new String(message.getMessage(), Charset.forName("UTF-8"));
+        return Optional.of(result);
+    }
+
     public static class Query {
-
-        public Query() {}
-
-        @XmlElement(required = true)
         public String topic;
-
-        @XmlElement
         public int depth;
+    }
+
+    public static class QueryResult {
+        public String topic;
+        public String value;
     }
 }
