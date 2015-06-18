d415f2c998adbbc573cd2b277be2732974d3060b
Guido Lorenz <guido.lorenz@artcom.de>
2015-06-18 15:35:47 +0200

    Refactor tests

--- test/test.js
+++ test/test.js
@@ -12,6 +12,7 @@ describe("HTTP API", function() {
     { topic: `${prefix}/topic1`, payload: "foo" },
     { topic: `${prefix}/topic2`, payload: "bar" }
   ];
+  const missingTopic = `${prefix}/does-not-exist`;
 
   let client;
 
@@ -36,29 +37,34 @@ describe("HTTP API", function() {
     client.end();
   });
 
-  const sendQuery = function(topic) {
-    const query = _.isArray(topic) ?
-                  _.map(topic, (t) => ({ topic: t })) :
-                  { topic };
-    return request.post(`http://${brokerUrl}:8080/query`, { json: query });
+  const rawQuery = function(json) {
+    return request.post(`http://${brokerUrl}:8080/query`, { json });
+  };
+
+  const singleQuery = function(topic, depth=null) {
+    const json = _.omit({ topic, depth }, _.isNull);
+    return rawQuery(json);
+  };
+
+  const batchQuery = function(topics) {
+    const json = _.map(topics, (topic) => ({ topic }));
+    return rawQuery(json);
   };
 
   describe("Single Queries", function() {
     it("should return the payload of a topic", function() {
       const { topic, payload } = data[0];
 
-      return sendQuery(topic).then((result) => {
+      return singleQuery(topic).then((result) => {
         expect(result).to.deep.equal({ topic, payload });
       });
     });
 
     it("should return error for inexistent topic", function() {
-      const topic = `${prefix}/does-not-exist`;
-
-      return sendQuery(topic).catch((error) => {
+      return singleQuery(missingTopic).catch((error) => {
         expect(error.response.statusCode).to.equal(404);
         expect(error.response.body).to.deep.equal({
-          topic,
+          topic: missingTopic,
           error: "NOT_FOUND"
         });
       });
@@ -69,18 +75,16 @@ describe("HTTP API", function() {
     it("should return the values of multiple topics", function() {
       const topics = _.map(data, "topic");
 
-      return sendQuery(topics).then((results) => {
+      return batchQuery(topics).then((results) => {
         expect(results).to.deep.equal(data);
       });
     });
 
     it("should return values and errors for multiple topics", function() {
-      const inexistent = `${prefix}/does-not-exist`;
-
-      return sendQuery([data[0].topic, inexistent]).then((results) => {
+      return batchQuery([data[0].topic, missingTopic]).then((results) => {
         expect(results).to.deep.equal([
           data[0],
-          { topic: inexistent, error: "NOT_FOUND" }
+          { topic: missingTopic, error: "NOT_FOUND" }
         ]);
       });
     });
