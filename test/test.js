50bfd3fc074d0c9b903b3c2f3fcb622f750b4ae3
Guido Lorenz <guido.lorenz@artcom.de>
2015-06-25 09:06:39 +0200

    Refactor error tests

--- test/test.js
+++ test/test.js
@@ -11,8 +11,13 @@ const expect = chai.expect;
 
 const BROKER_URL = process.env.BROKER || "localhost";
 
-function postQuery(json) {
-  return request.post(`http://${BROKER_URL}:8080/query`, { json });
+function postQuery(json, additionalOptions={}) {
+  const options = _.assign({ json }, additionalOptions);
+  return request.post(`http://${BROKER_URL}:8080/query`, options);
+}
+
+function postErrorQuery(json) {
+  return postQuery(json, { simple: false, resolveWithFullResponse: true });
 }
 
 describe("HTTP API", function() {
@@ -57,28 +62,27 @@ describe("HTTP API", function() {
     });
 
     it("should return error for inexistent topic", function() {
-      return postQuery({ topic: this.prefix + "/does-not-exist" }).then(() => {
-        throw new chai.AssertionError("Promise was expected to be rejected.");
-      }, (error) => {
-        expect(error.response.statusCode).to.equal(404);
-        expect(error.response.body).to.deep.equal({
-          topic: this.prefix + "/does-not-exist",
-          error: "NOT_FOUND"
-        });
+      const query = postErrorQuery({ topic: this.prefix + "/does-not-exist" });
+
+      return expect(query).to.eventually.include({
+        statusCode: 404
+      }).and.have.property("body").that.deep.equals({
+        topic: this.prefix + "/does-not-exist",
+        error: "NOT_FOUND"
       });
+
     });
 
     it("should return error for unpublished topic", function() {
-      return this.publish(this.prefix + "/topic1", null).then(() => {
-        return postQuery({ topic: this.prefix + "/topic1" });
-      }).then(() => {
-        throw new chai.AssertionError("Promise was expected to be rejected.");
-      }, (error) => {
-        expect(error.response.statusCode).to.equal(404);
-        expect(error.response.body).to.deep.equal({
-          topic: this.prefix + "/topic1",
-          error: "NOT_FOUND"
-        });
+      const query = this.publish(this.prefix + "/topic1", null).then(() => {
+        return postErrorQuery({ topic: this.prefix + "/topic1" });
+      });
+
+      return expect(query).to.eventually.include({
+          statusCode: 404
+      }).and.have.property("body").that.deep.equals({
+        topic: this.prefix + "/topic1",
+        error: "NOT_FOUND"
       });
     });
 
@@ -187,53 +191,49 @@ describe("HTTP API", function() {
 
   describe("Invalid Queries", function() {
     it("should return an error when topic is missing", function() {
-      return postQuery({ invalid: "query" }).then(() => {
-        throw new chai.AssertionError("Promise was expected to be rejected.");
-      }, (error) => {
-        expect(error.response.statusCode).to.equal(400);
-        expect(error.response.body).to.deep.equal({
-          error: "BAD_REQUEST",
-          message: "The response body must be a JSON object with a 'topic' and optional 'depth' property, or a JSON array of such objects."
-        });
+      const query = postErrorQuery({ invalid: "query" });
+
+      return expect(query).to.eventually.include({
+        statusCode: 400
+      }).and.to.have.property("body").that.deep.equals({
+        error: "BAD_REQUEST",
+        message: "The response body must be a JSON object with a 'topic' and optional 'depth' property, or a JSON array of such objects."
       });
     });
 
     it("should return an error when topic has trailing slash", function() {
-      return postQuery({ topic: "trailing/slash/" }).then(() => {
-        throw new chai.AssertionError("Promise was expected to be rejected.");
-      }, (error) => {
-        expect(error.response.statusCode).to.equal(400);
-        expect(error.response.body).to.deep.equal({
-          topic: "trailing/slash/",
-          error: "BAD_REQUEST",
-          message: "The topic cannot end with a slash."
-        });
+      const query = postErrorQuery({ topic: "trailing/slash/" });
+
+      return expect(query).to.eventually.include({
+        statusCode: 400
+      }).and.to.have.property("body").that.deep.equals({
+        topic: "trailing/slash/",
+        error: "BAD_REQUEST",
+        message: "The topic cannot end with a slash."
       });
     });
 
     it("should return an error when using multiple wildcards", function() {
-      return postQuery({ topic: "using/+/multiple/+/wildcards" }).then(() => {
-        throw new chai.AssertionError("Promise was expected to be rejected.");
-      }, (error) => {
-        expect(error.response.statusCode).to.equal(400);
-        expect(error.response.body).to.deep.equal({
-          topic: "using/+/multiple/+/wildcards",
-          error: "BAD_REQUEST",
-          message: "The topic cannot contain more than one wildcard."
-        });
+      const query = postErrorQuery({ topic: "using/+/multiple/+/wildcards" });
+
+      return expect(query).to.eventually.include({
+        statusCode: 400
+      }).and.to.have.property("body").that.deep.equals({
+        topic: "using/+/multiple/+/wildcards",
+        error: "BAD_REQUEST",
+        message: "The topic cannot contain more than one wildcard."
       });
     });
 
     it("should return an error for negative depth values", function() {
-      return postQuery({ topic: this.prefix, depth: -1 }).then(() => {
-        throw new chai.AssertionError("Promise was expected to be rejected.");
-      }, (error) => {
-        expect(error.response.statusCode).to.equal(400);
-        expect(error.response.body).to.deep.equal({
-          topic: this.prefix,
-          error: "BAD_REQUEST",
-          message: "The depth parameter cannot be negative."
-        });
+      const query = postErrorQuery({ topic: this.prefix, depth: -1 });
+
+      return expect(query).to.eventually.include({
+        statusCode: 400
+      }).and.to.have.property("body").that.deep.equals({
+        topic: this.prefix,
+        error: "BAD_REQUEST",
+        message: "The depth parameter cannot be negative."
       });
     });
   });
