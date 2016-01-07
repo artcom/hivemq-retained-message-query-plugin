bf97c73b623208e8900db35d0595445f910782d6
Guido Lorenz <guido.lorenz@artcom.de>
2016-01-07 09:19:28 +0100

    Bump version number after release

--- src/main/java/de/artcom/hivemq_http_api_plugin/HttpApiPluginModule.java
+++ src/main/java/de/artcom/hivemq_http_api_plugin/HttpApiPluginModule.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
-@Information(name = "HiveMQ HTTP API Plugin", author = "ART+COM AG", version = "0.4.2")
+@Information(name = "HiveMQ HTTP API Plugin", author = "ART+COM AG", version = "0.4.3-SNAPSHOT")
 public class HttpApiPluginModule extends HiveMQPluginModule {
 
     @Override
