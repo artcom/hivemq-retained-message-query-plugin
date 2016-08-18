367953c78728848821cf7c0e3762ab8c7aa59ce4
Guido Lorenz <guido.lorenz@artcom.de>
2016-08-18 16:06:57 +0200

    Remove redundant access modifiers

--- src/main/java/de/artcom/hivemq_http_api_plugin/query/results/Result.java
+++ src/main/java/de/artcom/hivemq_http_api_plugin/query/results/Result.java
@@ -3,6 +3,6 @@ package de.artcom.hivemq_http_api_plugin.query.results;
 import java.util.stream.Stream;
 
 public interface Result {
-    public int getStatus();
-    public abstract Stream<Result> flatten();
+    int getStatus();
+    Stream<Result> flatten();
 }
